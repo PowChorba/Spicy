036fce0172d9eb45b438e7380b7d80ef917daeee
PowChorba <101152834+PowChorba@users.noreply.github.com>
2023-05-15 13:55:17 -0300

    cookies

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,23 +1,15 @@
 'use client'
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Cookies from 'js-cookie'
 
 export default function Home() {
   const navigate = useRouter()
-  const local = window?.localStorage.getItem('edad')
 
   const handleEdad = () => {
-    localStorage.setItem('edad', 'true')
+    Cookies.set('edad', 'true')
     navigate.push('/inicio')
   }
   
-  useEffect(() => {
-    if (local === 'true') {
-      navigate.push('/inicio')
-    }
-  }, [local])
-
-
   
   return (
     <main className="w-2/3 flex-col justify-center items-center mx-auto my-20 border-2 border-grey-50 rounded-md p-8 shadow-[10px_10px_5px_-3px_rgba(0,0,0,0.22)] max-sm:w-full max-sm:my-0">
@@ -38,7 +30,7 @@ export default function Home() {
         </span>
       </div>
       <div className="py-4 text-center">
-      <button onClick={handleEdad} className="bg-[#D63423] text-white py-2 px-4 rounded-lg">Soy mayo de 18 años y entiendo los terminos y condiciones.</button>
+      <button onClick={handleEdad} className="bg-[#D63423] text-white py-2 px-4 rounded-lg">Soy mayor de 18 años y entiendo los terminos y condiciones.</button>
       </div>
     </main>
   );
--- client/middleware.ts
+++ client/middleware.ts
@@ -1,20 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
+import Cookies from "js-cookie";
 
+export async function middleware(request: NextRequest) {
+  const nextCookies = request.cookies.get("edad");
+  // console.log(nextCookies)
 
-export function middleware(request:NextRequest){
+  const requestHeader = new Headers(request.headers);
+  requestHeader.set("SpicyX", "Welcome");
 
-    const requestHeader = new Headers(request.headers)
-    requestHeader.set('Spicy-Tube', "Welcome")
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeader,
+    },
+  });
 
-    const response = NextResponse.next({
-        request: {
-            headers: requestHeader
-        }
-    })
+  if (request.nextUrl.pathname.endsWith("/video")) {
+    return NextResponse.redirect(new URL("/inicio", request.url));
+  }
 
-    if(request.nextUrl.pathname.endsWith('/video')) {
-        return NextResponse.redirect(new URL('/inicio', request.url))        
-    }
+  if (request.nextUrl.pathname.endsWith("/") && nextCookies?.value === "true") {
+    return NextResponse.redirect(new URL("/inicio", request.url));
+  }
+  if (
+    (request.nextUrl.pathname.includes("/categorias") ||
+      request.nextUrl.pathname.includes("/inicio") ||
+      request.nextUrl.pathname.includes("/pornstars")) &&
+    nextCookies?.value !== "true"
+  ) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 
-    return response
+  return response;
 }

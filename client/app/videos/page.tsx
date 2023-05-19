346ea0c690d73654d57a78f1c01d59be18ea87c1
PowChorba <101152834+PowChorba@users.noreply.github.com>
2023-05-19 18:04:20 -0300

    server side render en search title

--- client/app/videos/page.tsx
+++ client/app/videos/page.tsx
@@ -1,50 +1,49 @@
-"use client";
 import Navbar from "@/components/Navbar/Navbar";
-import VideoRender from "@/components/RenderVideos/VideosRender";
 import { VideoFormat } from "@/types";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 import { getData } from "./service/videos.service";
 import { DotSpinner } from "@uiball/loaders";
 import VideoRenderDos from "@/components/RenderVideos/VideosRenderDos";
 
-export default function SearchVideos({ param }: any) {
-  const search = useSearchParams();
-  const searchQuery = search?.get("title");
-  const [loader, setLoader] = useState(false);
-  const [state, setState] = useState<VideoFormat[]>([]);
+export default async function SearchVideos({ searchParams }: any) {
+  // const search = useSearchParams();
+  // const searchQuery = search?.get("title");
+  // const [loader, setLoader] = useState(false);
+  // const [state, setState] = useState<VideoFormat[]>([]);
+  console.log(searchParams, 'asdasdsdad')
+  
+  const data: VideoFormat[] = await getData(searchParams.title)
 
-  useEffect(() => {
-    const api = async () => {
-      const apiData = await getData(searchQuery);
-      setState(apiData);
-    };
-    const timer = () => {
-      setTimeout(() => {
-        setLoader(true);
-      }, 1000);
-    };
-    api();
-    timer();
-  }, [searchQuery]);
+  // useEffect(() => {
+  //   const api = async () => {
+  //     const apiData = await getData(searchQuery);
+  //     setState(apiData);
+  //   };
+  //   const timer = () => {
+  //     setTimeout(() => {
+  //       setLoader(true);
+  //     }, 1000);
+  //   };
+  //   api();
+  //   timer();
+  // }, [searchQuery]);
 
-  if (!loader) {
-    return (
-      <main>
-        <Navbar />
-        <div className="flex justify-center items-center p-20 font-bold text-[#D63423]">
-          <DotSpinner size={100} speed={0.9} color="#D63423" />
-        </div>
-      </main>
-    );
-  }
-  else if(state[0]?.title?.includes('Esta prohibido la busqueda y difunsion de videos relacionados a menores de edad. Porfavor no vuelvas a introduccir algo relacionado.')){
+  // if (!loader) {
+  //   return (
+  //     <main>
+  //       <Navbar />
+  //       <div className="flex justify-center items-center p-20 font-bold text-[#D63423]">
+  //         <DotSpinner size={100} speed={0.9} color="#D63423" />
+  //       </div>
+  //     </main>
+  //   );
+  // }
+  if(data[0]?.title?.includes('Esta prohibido la busqueda y difunsion de videos relacionados a menores de edad. Porfavor no vuelvas a introduccir algo relacionado.')){
     return(
       <main>
       <Navbar/>
       <div className="flex justify-center text-center items-center p-20 font-bold text-[#D63423]">
           {
-            state[0]?.title
+            data[0]?.title
           }
         </div>
     </main>
@@ -54,9 +53,9 @@ export default function SearchVideos({ param }: any) {
   return (
     <main>
       <Navbar />
-      {state?.length !== 0 ? (
+      {data?.length !== 0 ? (
         <>
-          <VideoRenderDos data={state} />
+          <VideoRenderDos data={data} />
         </>
       ) : (
         <div className="flex justify-center items-center p-20 font-bold text-[#D63423]">
